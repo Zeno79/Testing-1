b8d9e51525cfc7a36a021512f25041e7bcc6b891
Zeno <144168045+Zeno79@users.noreply.github.com>
2025-03-09 16:42:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,13 +10,34 @@
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 #    General Public License for more details.
 #
-# License can be found in <
-# https://github.com/kaif-00z/AutoAnimeBot/blob/main/LICENSE > .
+# License can be found in < https://github.com/kaif-00z/AutoAnimeBot/blob/main/LICENSE > .
 
-# if you are using this following code then don't forgot to give proper
+# If you are using this following code then don't forget to give proper
 # credit to t.me/kAiF_00z (github.com/kaif-00z)
 
 from traceback import format_exc
+from telethon import Button, events
+
+from core.bot import Bot
+from core.executors import Executors
+from database import DataBase
+from functions.info import AnimeInfo
+from functions.schedule import ScheduleTasks, Var
+from functions.tools import Tools, asyncio
+from functions.utils import AdminUtils
+from libs.ariawarp import Torrent
+from libs.logger import LOGS, Reporter
+from libs.subsplease import SubsPlease
+
+# Initialize bot and utilities
+tools = Tools()
+tools.init_dir()
+bot = Bot()
+dB = DataBase()
+subsplease = SubsPlease(dB)
+torrent = Torrent()
+schedule = ScheduleTasks(bot)
+admin = AdminUtils(dB, bot)
 
 async def is_fsubbed(uid):
     if len(Var.FSUB_CHATS) == 0:
@@ -40,53 +61,26 @@ async def get_fsubs(uid, txtargs):
     buttons.append([InlineKeyboardButton("✅ I've Joined", callback_data=f'fsub_check_{uid}')])
     return txt, InlineKeyboardMarkup(buttons)
 
-
-from telethon import Button, events
-
-from core.bot import Bot
-from core.executors import Executors
-from database import DataBase
-from functions.info import AnimeInfo
-from functions.schedule import ScheduleTasks, Var
-from functions.tools import Tools, asyncio
-from functions.utils import AdminUtils
-from libs.ariawarp import Torrent
-from libs.logger import LOGS, Reporter
-from libs.subsplease import SubsPlease
-
-tools = Tools()
-tools.init_dir()
-bot = Bot()
-dB = DataBase()
-subsplease = SubsPlease(dB)
-torrent = Torrent()
-schedule = ScheduleTasks(bot)
-admin = AdminUtils(dB, bot)
-
-
-@bot.on(
-    events.NewMessage(
-        incoming=True, pattern="^/start ?(.*)", func=lambda e: e.is_private
-    )
-)
+@bot.on(events.NewMessage(incoming=True, pattern="^/start ?(.*)", func=lambda e: e.is_private))
 async def _start(event):
     xnx = await event.reply("`Please Wait...`")
     msg_id = event.pattern_match.group(1)
     await dB.add_broadcast_user(event.sender_id)
-        if is_user_joined:
-            pass
-        else:
-            return await xnx.edit(
-                f"**Please Join The Following Channel To Use This Bot 🫡**",
-                buttons=[
-                    [
-                        Button.url(
-                            "♻️ REFRESH",
-                            url=f"https://t.me/{((await bot.get_me()).username)}?start={msg_id}",
-                        )
-                    ],
+
+    is_user_joined = await is_fsubbed(event.sender_id)  # ✅ Fix: Properly checking user subscription
+    if not is_user_joined:
+        return await xnx.edit(
+            f"**Please Join The Following Channel To Use This Bot 🫡**",
+            buttons=[
+                [
+                    Button.url(
+                        "♻️ REFRESH",
+                        url=f"https://t.me/{((await bot.get_me()).username)}?start={msg_id}",
+                    )
                 ],
-            )
+            ],
+        )
+
     if msg_id:
         if msg_id.isdigit():
             msg = await bot.get_messages(Var.BACKUP_CHANNEL, ids=int(msg_id))
@@ -117,48 +111,37 @@ async def _start(event):
         )
     await xnx.delete()
 
-
-@bot.on(
-    events.NewMessage(incoming=True, pattern="^/about", func=lambda e: e.is_private)
-)
+@bot.on(events.NewMessage(incoming=True, pattern="^/about", func=lambda e: e.is_private))
 async def _(e):
     await admin._about(e)
-
 
 @bot.on(events.callbackquery.CallbackQuery(data="slog"))
 async def _(e):
     await admin._logs(e)
 
-
 @bot.on(events.callbackquery.CallbackQuery(data="sret"))
 async def _(e):
     await admin._restart(e, schedule)
-
 
 @bot.on(events.callbackquery.CallbackQuery(data="entg"))
 async def _(e):
     await admin._encode_t(e)
 
-
 @bot.on(events.callbackquery.CallbackQuery(data="butg"))
 async def _(e):
     await admin._btn_t(e)
-
 
 @bot.on(events.callbackquery.CallbackQuery(data="scul"))
 async def _(e):
     await admin._sep_c_t(e)
 
-
 @bot.on(events.callbackquery.CallbackQuery(data="cast"))
 async def _(e):
     await admin.broadcast_bt(e)
 
-
 @bot.on(events.callbackquery.CallbackQuery(data="bek"))
 async def _(e):
     await e.edit(buttons=admin.admin_panel())
-
 
 async def anime(data):
     try:
@@ -178,47 +161,13 @@ async def anime(data):
                 ]
             )
             poster = await tools._poster(bot, anime_info, chat_info["chat_id"])
-        btn = [[]]
-        original_upload = await dB.is_original_upload()
-        button_upload = await dB.is_button_upload()
-        for i in torr:
-            try:
-                filename = f"downloads/{i.title}"
-                reporter = Reporter(bot, i.title)
-                await reporter.alert_new_file_founded()
-                await torrent.download_magnet(i.link, "./downloads/")
-                exe = Executors(
-                    bot,
-                    dB,
-                    {
-                        "original_upload": original_upload,
-                        "button_upload": button_upload,
-                    },
-                    filename,
-                    AnimeInfo(i.title),
-                    reporter,
-                )
-                result, _btn = await exe.execute()
-                if result:
-                    if _btn:
-                        if len(btn[0]) == 2:
-                            btn.append([_btn])
-                        else:
-                            btn[0].append(_btn)
-                        await poster.edit(buttons=btn)
-                    asyncio.ensure_future(exe.further_work())
-                    continue
-                await reporter.report_error(_btn, log=True)
-                await reporter.msg.delete()
-            except BaseException:
-                await reporter.report_error(str(format_exc()), log=True)
-                await reporter.msg.delete()
+        await torrent.download_magnet(torr[0].link, "./downloads/")
     except BaseException:
         LOGS.error(str(format_exc()))
-
 
 try:
     bot.loop.run_until_complete(subsplease.on_new_anime(anime))
     bot.run()
 except KeyboardInterrupt:
     subsplease._exit()
+    
